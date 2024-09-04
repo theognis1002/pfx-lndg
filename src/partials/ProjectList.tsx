72e14c6312607dc29b3d32b8f32b5abfd55e09a3
theognis1002 <theognis1002@gmail.com>
2024-09-04 17:13:21 -0400

    Update logo + add Projects component

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -10,56 +10,42 @@ const ProjectList = () => (
   <Section
     title={
       <>
-        <GradientText>Recent Projects</GradientText>
+        <GradientText>Projects</GradientText>
       </>
     }
   >
     <div className="flex flex-col gap-6">
       <Project
-        name="Limee"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
+        name="Limee AI"
+        description="Limee provides innovative Voice AI agents that transform how restaurants receive phone calls and handle orders. 
+        By leveraging open-source large language models (LLMs) technology offers a smart, seamless alternative to traditional staffing and call center solutions - enhancing both efficiency and customer satisfaction."
         link="/"
         img={{
-          src: '/assets/images/project-web-design.png',
-          alt: 'Project Web Design',
+          src: '/assets/images/limee.png',
+          alt: 'Limee AI',
         }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.LIME}>Web design</Tags>
-            <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
+            <Tags color={ColorTags.STONE}>Next.js</Tags>
             <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.GREEN}>Django</Tags>
+            <Tags color={ColorTags.LIME}>Celery</Tags>
+            <Tags color={ColorTags.RED}>Redis</Tags>
+            <Tags color={ColorTags.GRAY}>PostgreSQL</Tags>
           </>
         }
       />
       <Project
-        name="TicketBrokerList"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
+        name="Spotify Clone - Seapig Tickets"
+        description="Spotify Clone for Seapig Tickets. This project is a clone of the Spotify Web Player. It is a full-stack application that uses the Spotify Web API to fetch data and play music. The front-end is built with TypeScript & React."
+        link="https://www.seapigtickets.com"
+        img={{
+          src: '/assets/images/spotify.png',
+          alt: 'Spotify Clone - Seapig Tickets',
+        }}
         category={
           <>
-            <Tags color={ColorTags.VIOLET}>Next.js</Tags>
-            <Tags color={ColorTags.EMERALD}>Blog</Tags>
-            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
-          </>
-        }
-      />
-      <Project
-        name="Spotify"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{ src: '/assets/images/project-maps.png', alt: 'Project Maps' }}
-        category={
-          <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.INDIGO}>Bootstrap</Tags>
+            <Tags color={ColorTags.STONE}>Next.js</Tags>
             <Tags color={ColorTags.ROSE}>TypeScript</Tags>
           </>
         }
